94c8775e7096313e36dcf4c327cc713d4ec4c7c1
biancarosa <me@biancarosa.com.br>
2025-03-23 12:13:30 -0300

    observability: add otel exporter

--- api/app/healthcheck/healthcheck.py
+++ api/app/healthcheck/healthcheck.py
@@ -4,13 +4,16 @@ Module that deals with HealthCheck route."""
 import logging
 
 from flask import jsonify
+from opentelemetry import trace
 
 log = logging.getLogger(__name__)
+tracer = trace.get_tracer(__name__)
 
 
 def route():
     """Returns health information"""
-    log.info("Healthcheck was requested")
-    return jsonify({
-        "message": "Hello. Is it me you're looking for?"
-    })
+    with tracer.start_as_current_span("healthcheck_route") as span:
+        span.set_attribute("service.name", "healthcheck")
+        return jsonify({
+            "message": "Hello. Is it me you're looking for?"
+        })
