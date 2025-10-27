8d4c12b80d3693d7eca6c9f9212411e2b6c95059
Bianca Rosa <me@biancarosa.com.br>
2025-10-27 19:20:26 -0300

    Remove pylint disable comment - package is now in lock file

--- api/app/main.py
+++ api/app/main.py
@@ -7,7 +7,7 @@ import logging
 
 from flask import Flask
 from flask_cors import CORS
-from opentelemetry.instrumentation.logging import LoggingInstrumentor  # pylint: disable=import-error,no-name-in-module
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
